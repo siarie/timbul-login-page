493808374c4a5f4225e3141d198c2dc661e5b9f5
Sri Aspari <sriaspari@gmail.com>
2020-06-13 11:12:11 +0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+var gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  nunjucksRender = require("gulp-nunjucks"),
+  rename = require("gulp-rename"),
+  cleanCSS = require("gulp-clean-css"),
+  browserSync = require("browser-sync").create();
+
+var paths = {
+  input: {
+    dir: "./src/",
+    scss: "./src/scss/main.scss",
+    html: "./src/html/*.html",
+  },
+  output: {
+    html: "./public",
+    css: "./public/css",
+  },
+  watch: {
+    public: "./public/**/*.{html,css}",
+    sass: "./src/scss/**/*.scss",
+  },
+};
+
+function style() {
+  return gulp
+    .src(paths.input.scss)
+    .pipe(
+      sass({
+        outputStyle: "nested",
+      }).on("error", sass.logError)
+    )
+    .pipe(gulp.dest(paths.output.css))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(paths.output.css))
+    .pipe(browserSync.stream());
+}
+
+function html() {
+  return gulp
+    .src(paths.input.html)
+    .pipe(nunjucksRender.compile())
+    .pipe(gulp.dest(paths.output.html));
+}
+
+function startServer(cb) {
+  browserSync.init({
+    server: {
+      baseDir: paths.output.html,
+    },
+  });
+
+  cb();
+}
+
+function reloadServer(cb) {
+  browserSync.reload();
+  cb();
+}
+
+function watch() {
+  gulp.watch(paths.input.dir, gulp.series(style, html, reloadServer));
+}
+
+exports.default = gulp.series(style, html, startServer, watch);
